28b74b7f56d604b8f234766781a4518ee1959c4a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-13 19:05:35 +0500

    footer made for future use

--- reactfooter.js
+++ reactfooter.js
@@ -1,4 +1,3 @@
-importing
 import {
   BsGithub,
   BsYoutube,
@@ -10,67 +9,142 @@ import {
 import { HiOutlineMail } from "react-icons/hi";
 import { FaHackerrank } from "react-icons/fa";
 
-
-html
-<footer>
+const FooterLogo = () => {
+  return (
+    <>
+      <footer>
         <p>Developed by Shehzad</p>
-		<ul className="iconscontainer">
-            <li><a href="https://twitter.com/Shehza_d_" target="_blank" rel="noreferrer" > <BsTwitter/></a></li>
-            <li><a href="https://github.com/shehza-d" target="_blank" rel="noreferrer"  > <BsGithub/></a></li>
-            <li><a href="https://www.linkedin.com/in/shehzadd/" target="_blank" rel="noreferrer"> <BsLinkedin/> </a></li>
-            <li><a href="https://instagram.com/shehza.d?utm_medium=copy_link" target="_blank" rel="noreferrer" > <BsInstagram/></a></li>
-            <li><a href="https://www.youtube.com/channel/UCUTMs216kmgY9lOgBFDckAQ" target="_blank" rel="noreferrer" ><BsYoutube/></a></li>
-            <li><a href="mailto:shehzaddiqbal@outlook.com" target="_blank"  rel="noreferrer" > <HiOutlineMail/></a></li>
-            <li><a href="https://www.hackerrank.com/shehza_d" target="_blank"  rel="noreferrer" > <FaHackerrank/></a></li>
-            <li><a href="https://stackoverflow.com/users/18210334/shehzad" target="_blank"  rel="noreferrer" > <BsStackOverflow/></a></li>
-          
-            {/* <li><a href="https://www.fiverr.com/shehza_d" target="_blank"  rel="noreferrer" > <i><FaHackerrank/></i></a></li>
+        <ul className="iconscontainer">
+          <li>
+            <a
+              href="https://twitter.com/Shehza_d_"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <BsTwitter />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/shehza-d"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <BsGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/shehzadd/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <BsLinkedin />{" "}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://instagram.com/shehza.d?utm_medium=copy_link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <BsInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/channel/UCUTMs216kmgY9lOgBFDckAQ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsYoutube />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:shehzaddiqbal@outlook.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <HiOutlineMail />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.hackerrank.com/shehza_d"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <FaHackerrank />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://stackoverflow.com/users/18210334/shehzad"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <BsStackOverflow />
+            </a>
+          </li>
+
+          {/* <li><a href="https://www.fiverr.com/shehza_d" target="_blank"  rel="noreferrer" > <i><FaHackerrank/></i></a></li>
             <li><a href="hackerrank.com" target="_blank"  rel="noreferrer" > <i><FaHackerrank/></i></a></li>
             <li><a href="hackerrank.com" target="_blank"  rel="noreferrer" > <i><FaHackerrank/></i></a></li> */}
         </ul>
-    </footer>
-CSS
-/* Footer/links */
-.iconscontainer {
-    list-style: none;
-    display: flex;
-    fill: #ffffff;
-    margin: 15px;
-    /* width: 20px; */
-}
+      </footer>
+    </>
+  );
+};
 
-.icon {
-    padding-right: 13px;
-    fill: white;
-    width: 18px;
-    cursor: pointer;
-}
+export default FooterLogo;
+// CSS
+// /* Footer/links */
+// .iconscontainer {
+//     list-style: none;
+//     display: flex;
+//     fill: #ffffff;
+//     margin: 15px;
+//     /* width: 20px; */
+// }
 
-.icon:hover {
-    fill: #fffc3a;
-}
+// .icon {
+//     padding-right: 13px;
+//     fill: white;
+//     width: 18px;
+//     cursor: pointer;
+// }
 
-footer {
+// .icon:hover {
+//     fill: #fffc3a;
+// }
 
-    /* position: sticky; */
-    /* position: absolute; */
-    /* display: hidden; */
-    display: flex;
-    justify-content: center;
-    /* bottom: 0; */
-    width: 100%;
-    height: 25px;
-    background: rgb(14, 154, 224);
-    padding: 18px 0px;
-    align-items: center;
-}
+// footer {
 
+//     /* position: sticky; */
+//     /* position: absolute; */
+//     /* display: hidden; */
+//     display: flex;
+//     justify-content: center;
+//     /* bottom: 0; */
+//     width: 100%;
+//     height: 25px;
+//     background: rgb(14, 154, 224);
+//     padding: 18px 0px;
+//     align-items: center;
+// }
 
-footer>p {
-    color: white;
-    /* padding-top: 15px; */
-    /* padding-bottom: 15px; */
-    font-size: 20px;
-} 
-/* Footer/links end*/
-
+// footer>p {
+//     color: white;
+//     /* padding-top: 15px; */
+//     /* padding-bottom: 15px; */
+//     font-size: 20px;
+// }
+// /* Footer/links end*/
